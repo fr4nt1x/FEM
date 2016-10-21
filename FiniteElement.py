e159d839d19886e39d59e255b310032d1e784410
fra <fra@debian>
2016-10-21 18:04:53 +0100

    should wor with right hand side, but not error checke

--- FiniteElement.py
+++ FiniteElement.py
@@ -1,5 +1,6 @@
 from scipy.spatial import Delaunay
 from scipy import sparse
+from scipy.sparse import linalg
 from itertools import product
 import math
 
@@ -7,20 +8,30 @@ import numpy as np
 
 class FiniteElement:
     """Initiates the triangulation and calculates the stiffness matrix and righthand side"""
-    def __init__(self,points,PDEMatrix=np.eye(2)):
+    def __init__(self,points,prescribedValues,PDEMatrix=np.eye(2),functionRHS=lambda x: 0):
         #referenceElement holds the points of the reference element from which all other elements
         #are calculated
+        self.functionRHS= functionRHS
         self.referenceElement = np.array([[0,0],[1.,0],[0,1]])
         self.triangulation = Delaunay(points)
+        self.numberDOF = np.size(self.triangulation.points[:,0])
+        
+        #self.findIndicesForPrescribedValues(prescribedValues)
+        self.prescribedValues = prescribedValues
         self.linearBasis = []
         self.linearBasis.append(lambda x,y : 1-x-y)
         self.linearBasis.append(lambda x,y : x)
         self.linearBasis.append(lambda x,y : y)
+
+        #Holds integral of two basisfunctons in one reference triangle
+        self.elementaryBasisMatrix = 1/12*np.array([[1,0.5,0.5],[0.5,1,0.5],[0.5,0.5,1]])
+        
         self.gradBasis = []
         self.gradBasis.append(np.array([-1,-1])) 
         self.gradBasis.append(np.array([1,0]))
         self.gradBasis.append(np.array([0,1]))
-        
+        self.rightHandSide = np.zeros(self.numberDOF)
+
         self.PDEMatrix= PDEMatrix
 
     def calculateTransform(self,triangleIndex):
@@ -33,7 +44,7 @@ class FiniteElement:
         #hold the coordinates with 1 appended as last row
         referenceCoord = np.array([self.referenceElement[:,0],self.referenceElement[:,1],np.array([1,1,1])])       
         transformedCoord = np.array([trianglePoints[:,0],trianglePoints[:,1],np.array([1,1,1])])
-        C = np.dot( transformedCoord,np.linalg.inv(referenceCoord))
+        C = np.dot( transformedCoord,np.linalg.inv(referenceCoord)) 
         #L is in the first n x n submatrix of (n+1) x (n+1) Matrix C, b the last column without the last entry 
         return (C[0:-1,0:-1],C[:-1,-1])
 
@@ -55,20 +66,49 @@ class FiniteElement:
 
     def calculateGlobalStiffnessMatrix(self):
         """Loops over all elements, calculates the element Stiffness and assembles 
-            it inCSRto a global Matrix"""
+            it in CSC to a global Matrix"""
         
         globalRowIndices = []
         globalColumnIndices=[]
         globalData = []
         for ele,triPoints in enumerate(self.triangulation.simplices):
+            print("element:",ele)
             eleStiffness = self.calculateElementStiffnessMatrix(ele)
-            for row,column in product(range(3),range(3)):
-                globalRowIndices.append(triPoints[row])
-                globalColumnIndices.append(triPoints[column])
-                globalData.append(eleStiffness[row,column])
-        self.GlobalStiffness = sparse.coo_matrix((globalData,(globalRowIndices,globalColumnIndices)),shape=(np.size(self.triangulation.points),np.size(self.triangulation.points))).tocsr() 
-        print(self.GlobalStiffness.toarray())
-            
+            for row in range(3):
+                if triPoints[row] in self.prescribedValues[:,0]:
+                    continue
+                else:
+                    for column in range(3):
+                        globalRowIndices.append(triPoints[row])
+                        globalColumnIndices.append(triPoints[column])
+                        globalData.append(eleStiffness[row,column])
+        for v in self.prescribedValues:
+            globalRowIndices.append(v[0])
+            globalColumnIndices.append(v[0])
+            globalData.append(1)
+        self.GlobalStiffness = sparse.coo_matrix((globalData,(globalRowIndices,globalColumnIndices))).tocsc() 
+
+       # print(self.GlobalStiffness.toarray())
+
+    def calculateRightHandSide(self):
+        for ele,triPoints in enumerate(self.triangulation.simplices):
+            elementRHS = self.calculateElementRightHandSide(ele)
+            for index,entry in enumerate(elementRHS):
+                self.rightHandSide[triPoints[index]] += entry
+        for indexValue in self.prescribedValues:
+            self.rightHandSide[indexValue[0]] = indexValue[1]
+        print("RHS",self.rightHandSide)
+    
+    def calculateElementRightHandSide(self,triangleIndex):
+        transformMatrix,translateVector = self.calculateTransform(triangleIndex)
+        determinant = np.linalg.det(transformMatrix)
+        trianglePoints =self.triangulation.points[self.triangulation.simplices.copy()[triangleIndex]]
+        elementRHS = determinant*np.dot(self.elementaryBasisMatrix,np.array([self.functionRHS(x) for x in trianglePoints] ))
+        return elementRHS
+
+    def solve(self):
+        self.solution =sparse.linalg.spsolve(self.GlobalStiffness,self.rightHandSide)
+        
     def Global2Local(self,globalIndex):
         """returns for a given global index the pair of local ones"""
         ele = int(math.floor(globalIndex / 3))
--- test.py
+++ test.py
@@ -1,13 +1,44 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 import numpy as np
+import math
+from numpy import linalg
 from FiniteElement import FiniteElement
 
+nx =50
+ny =50
+dx = 1/(nx-1)
+dy= 1/(ny-1)
+points = []
+for x in np.linspace(-1.5,1.5,nx):
+    for y in np.linspace(-1.5,1.5,ny):
+        points.append([x,y])
+#points = [x  for x in points if linalg.norm(x)<=1]
+boundaryValuesx = []#[([x,-1.5],0) for x in np.linspace(-1.5,1.5,nx)]
+boundaryValuesy = []#[([0,y],0) for y in np.linspace(0,1,ny)]
+boundaryRadial = [[k,0] for k,x in enumerate(points) if linalg.norm(x) >1]
+boundaryRadial +=[]# [[k,0] for k,x in enumerate(points) if linalg.norm(x) <=0.3]
+boundaryValuesx += []#[[x,1] for x in np.array(points)[:,-1]]
+boundaryValuesy += []#[[x,0] for x in np.array(points)[-1,:]]
 
-Fem = FiniteElement(np.array([[1.,1],[0,2],[0,1],[0,0]]))
-print(Fem.triangulation)
+f= lambda x : x[0]
+
+boundary = np.array(boundaryValuesx + boundaryValuesy+boundaryRadial)
+Fem = FiniteElement(points,boundary,PDEMatrix= np.array([[1,0],[0,10]]),functionRHS = f)
 Fem.calculateElementStiffnessMatrix(0)
 Fem.calculateGlobalStiffnessMatrix()
+Fem.calculateRightHandSide()
+Fem.solve()
+#plt.triplot(Fem.triangulation.points[:,0],Fem.triangulation.points[:,1],Fem.triangulation.simplices.copy())
+#plt.plot(Fem.triangulation.points[:,0],Fem.triangulation.points[:,1],'o')
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_trisurf(Fem.triangulation.points[:,0],Fem.triangulation.points[:,1],Fem.triangulation.simplices.copy(),Fem.solution)
+
+#
+#print(Fem.triangulation.points[:])
+plt.show()
 
 #rhs = lambda x,y: -0.5*x+-0.5*y+0.5 
 #fig = plt.figure()
